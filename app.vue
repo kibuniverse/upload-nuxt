95b68a6bb2d13d910674d20a1b4023f4f7c04a1f
yankaizhi <yankaizhi.uni@bytedance.com>
2023-07-10 22:02:03 +0800

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data } = await useFetch("/api/hello");
+console.log(data, "data");
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+    <pre>{{ data.hello }}</pre>
+  </div>
+</template>
