954a36eef0a4a3692be4757bb24a43eb7527e894
yankaizhi <yankaizhi.uni@bytedance.com>
2023-07-13 19:23:50 +0800

    chore: perf ui

--- app.vue
+++ app.vue
@@ -1,11 +1,29 @@
 <script setup>
 const { data } = await useFetch("/api/hello");
-console.log(data, "data");
 </script>
 
 <template>
-  <div>
-    <NuxtWelcome />
-    <pre>{{ data.hello }}</pre>
-  </div>
+  <div class="container"><span>Drag and drop here</span></div>
 </template>
+
+<style>
+.container {
+  min-width: 100px;
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  height: 30vh;
+  border: 1px solid gray;
+  border-radius: 4px;
+}
+body {
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
