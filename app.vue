eece55e795ed672247c5a491cbdd382bf3984561
kibuniverse <3050540005@qq.com>
2023-07-15 16:25:35 +0800

    docs: update readme

--- app.vue
+++ app.vue
@@ -3,27 +3,30 @@ const { data } = await useFetch("/api/hello");
 </script>
 
 <template>
-  <div class="container"><span>Drag and drop here</span></div>
+    <div class="container">
+        <span>Drag and drop here</span>
+    </div>
 </template>
 
 <style>
 .container {
-  min-width: 100px;
-  width: 30vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0 auto;
-  height: 30vh;
-  border: 1px solid gray;
-  border-radius: 4px;
+    min-width: 100px;
+    width: 30vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    height: 30vh;
+    border: 1px solid gray;
+    border-radius: 4px;
 }
+
 body {
-  margin: 0;
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    margin: 0;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
--- server/api/merge.ts
+++ server/api/merge.ts
@@ -1,5 +1,4 @@
 export default defineEventHandler((event) => {
-  console.log(event);
   return {
     hello: "world",
   };
